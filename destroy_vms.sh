80ed671815282c11f34bc39a75fe53a85dab1b3e
Ian Miell <ian.miell@gmail.com>
2016-05-14 17:27:08 +0100

    destro

--- destroy_vms.sh
+++ destroy_vms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true 
 do
-	VBoxManage list runningvms | grep docker_101_tutorial | awk '{print $1}' | xargs --no-run-if-empty -IXXX VBoxManage controlvm 'XXX' poweroff && VBoxManage list vms | awk '{print $1}'  | xargs --no-run-if-empty -IXXX VBoxManage unregistervm 'XXX' --delete
+	VBoxManage list runningvms | grep docker_101_tutorial | awk '{print $1}' | xargs -IXXX VBoxManage controlvm 'XXX' poweroff && VBoxManage list vms | grep docker_101_tutorial || awk '{print $1}'  | xargs -IXXX VBoxManage unregistervm 'XXX' --delete
 	if [[ $(VBoxManage list vms | grep docker_101_tutorial | wc -l) == '0' ]]
 	then
 		break
